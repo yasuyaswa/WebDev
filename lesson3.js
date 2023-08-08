bbe22048d213b4fc91d74cbf857407c25723146d
yaswanth <yasuyaswa@gmail.com>
2023-08-08 22:06:59 +0530

    useless things are removed

--- lesson3.js
+++ lesson3.js
@@ -1,2 +1,50 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Nice, You have saved the Nature', 'success')
+  })
+}
+
+
+const alertPlaceholder2 = document.getElementById('liveAlertPlaceholder2')
+const appendAlert2 = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder2.append(wrapper)
+}
+
+const alertTrigger2 = document.getElementById('liveAlertBtn2')
+if (alertTrigger2) {
+  alertTrigger2.addEventListener('click', () => {
+    appendAlert2('Please, Save the Nature','danger')
+  })
+}
+
+//$("btnAcc").html('You have Saved Nature')
+function Accept(){
+    // window.alert("You have Saved Nature");
+    var a =$("#alert1").html("You have Saved Nature");
+//     window.alert($("#alert1").html("You have Saved Nature"));
+}
